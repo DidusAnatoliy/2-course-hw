f1f5526685d16bf06f24e2af18fc3f358f630e43
bereg.007@mail.ru <you@example.com>
2024-02-13 19:28:54 +0300

    add correction

--- hw5.js
+++ hw5.js
@@ -17,99 +17,107 @@ game1.addEventListener("click", () => {
 })
 
 // Task 1
-// function minOfTwoNumbers(num1, num2) {
-//     return Math.min(num1, num2);
-// }
-// console.log(minOfTwoNumbers(8, 4)); 
-// console.log(minOfTwoNumbers(6, 6)); 
+
+function minOfTwoNumbers(d, b) {
+    return d < b ? d : b;
+}
+console.log(minOfTwoNumbers(8, 4));
+console.log(minOfTwoNumbers(6, 6));
 
 // Task 2
-// function checkEvenOrOdd(number) {
-//     if (number % 2 === 0) {
-//         return 'Число четное';
-//     } else {
-//         return 'Число нечетное';
-//     }
-// }
-// console.log(checkEvenOrOdd(4)); 
-// console.log(checkEvenOrOdd(7)); 
+
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        return 'Число четное';
+    } else {
+        return 'Число нечетное';
+    }
+}
+console.log(checkEvenOrOdd(4));
+console.log(checkEvenOrOdd(7));
 
 // Task 3
-// 3.1
-// function square(a) {
-//     a = a ** 2
-//     console.log(a)
-// }
-// let a = Number(prompt("Введите число", 32))
-// square(a)
-// 3.2
-// function square(a) {
-//     a = a ** 2
-//     return a
-// }
-// let a = Number(prompt("Введите число", 32))
-// console.log(square(a))
 
-// // Task 4
+3.1
 
-// function userAge(age) {
-//     if (age < 0) {
-//         console.log("Вы ввели неправильное значение.");
-//     } else if (age >= 0 && age <= 12) {
-//         console.log("Привет, друг!");
-//     } else if (age >= 13) {
-//         console.log("Добро пожаловать!");
-//     }
-// }
-//   let age = Number(prompt("Сколько вам лет?"))
+function square(c) {
+    c = c ** 2
+    console.log(c)
+}
+let c = Number(prompt("Введите число", 32))
+square(c)
 
-//   userAge(age)
+3.2
 
-// // Task 5
+function square(a) {
+    a = a ** 2
+    return a
+}
+let a = Number(prompt("Введите число", 32))
+console.log(square(a))
 
-//   function sum (a, b) {
-//     if (isNaN(a) || isNaN(b)){
-//         console.log("Одно или оба значения не являются числом")
-//     } else if (sum = a + b) {
-//         console.log(sum)
-//     }
-//   }
+// Task 4
 
-//   let a = Number(prompt("Введите первое число"))
-//   let b = Number(prompt("Введите второе число"))
+function userAge(age) {
+    if (age < 0) {
+        console.log("Вы ввели неправильное значение.");
+    } else if (age >= 0 && age <= 12) {
+        console.log("Привет, друг!");
+    } else  {
+        console.log("Добро пожаловать!");
+    }  
+}
 
-// // Task 6
+let age = Number(prompt("Сколько вам лет?"))
+
+userAge(age)
+
+// Task 5
+
+function result(r, b) {
+    if (isNaN(r) || isNaN(b)) {
+        console.log("Одно или оба значения не являются числом")
+    } else if (result = r * b) {
+        console.log(result)
+    }
+}
+
+let r = Number(prompt("Введите первое число"))
+let b = Number(prompt("Введите второе число"))
+
+// Task 6
+
+function cube(userNum) {
+    if (isNaN(userNum)) {
+        console.log("Переданный параметр не является числом");
+    } else {
+        let cubeNum = userNum ** 3;
+        console.log(`${userNum} в кубе равняется ${cubeNum}`);
+    }
+}
+
+let userNum = Number(prompt("Введите число"));
+
+cube(userNum);
+
+// Task 7
+
+function Circle(radius) {
+    this.radius = radius;
+    this.getArea = function () {
+        return Math.PI * this.radius * this.radius;
+    };
+    this.getPerimeter = function () {
+        return 2 * Math.PI * this.radius;
+    };
+}
 
 
-// function cube(userNum) {
-//     if (isNaN(userNum)) {
-//         console.log("Переданный параметр не является числом")
-//     } else if (cubeNum = userNum ** 3){
-// console.log(`${userNum} в кубе ровняется ${cubeNum} `)
-// }
-// }
-// let userNum = Number(prompt("Введите число"))
+let circle1 = new Circle(5);
+let circle2 = new Circle(10);
 
-// cube(userNum)
 
-// // Task 7
-// function Circle(radius) {
-//     this.radius = radius;
-//     this.getArea = function() {
-//       return Math.PI * this.radius * this.radius;
-//     };
-//     this.getPerimeter = function() {
-//       return  2 * Math.PI * this.radius;
-//     };
-//   }
-  
-  
-//   let circle1 = new Circle(5);
-//   let circle2 = new Circle(10);
-  
-  
-//   console.log(circle1.getArea());       
-//   console.log(circle1.getPerimeter());  
-//   console.log(circle2.getArea());       
-//   console.log(circle2.getPerimeter());  
-  
+console.log(circle1.getArea());
+console.log(circle1.getPerimeter());
+console.log(circle2.getArea());
+console.log(circle2.getPerimeter());
