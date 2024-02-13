b763a894af959fafea1757a62111851d6c253e28
bereg.007@mail.ru <you@example.com>
2024-02-13 21:51:19 +0300

    Add corecction

--- hw5.js
+++ hw5.js
@@ -1,17 +1,18 @@
 // Task 8
+const game1 = document.getElementById("game1")
 game1.addEventListener("click", () => {
     const userNumMounth = Number(prompt("Введите номер месяца"))
     if (isNaN(userNumMounth)) {
         console.log("Запрос некорректный");
-    } else if (userNumMounth === 0 || userNumMounth >= 13) {
+    } else if (userNumMounth <= 0 || userNumMounth >= 13) {
         console.log("Неверный  номер месяца. Введите число от  1 до  12.")
     } else if (userNumMounth === 12 || userNumMounth === 1 || userNumMounth === 2) {
         console.log("Зима")
-    } else if (userNumMounth === 3 || userNumMounth === 4 || userNumMounth === 5) {
+    } else if (userNumMounth >=  3 && userNumMounth <=  5) {
         console.log("Весна")
-    } else if (userNumMounth === 6 || userNumMounth === 7 || userNumMounth === 8) {
+    } else if (userNumMounth >=  6 && userNumMounth <=  8) {
         console.log("Лето")
-    } else if (userNumMounth === 9 || userNumMounth === 10 || userNumMounth === 11) {
+    } else if (userNumMounth >= 9 && userNumMounth <= 11) {
         console.log("Осень")
     }
 })
